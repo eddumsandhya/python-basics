56b8727e1d1aa6f343df5ec2c1422040ef6fdc16
eddumsandhya <Sandhyaeddum2403@gmail.com>
2026-09-17 16:18:04 +0530

    Implement arithmetic and comparison operations
    Added various arithmetic and comparison operations with user input.

---
+++ E.Sandhya.py
@@ -0,0 +1,36 @@
+
+=10
+b=15
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)#3
+
+a=10
+a +=10
+a -=10
+a -=20a
+print(a-a)
+a=int(input("enter your value"))
+b=int(input("enter your value"))
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+x=int(input("enter the a value :"))
+y=int(input("enter the b value :"))
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x<=y)
+print(x>=y)
+10==10 and 10>20
+10==10 or 30>20
+not(10==20 or 30>40)
+
+
